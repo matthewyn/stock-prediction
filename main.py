b549d1faa8af4b173681d837e00676941c8bd63f
Kenneth Matthew <programmer.kenneth@gmail.com>
2024-03-30 15:56:53 +0700

    Add decision tree regression

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
 
-df = pd.read_csv("GJTL_Yearly.csv")
+df = pd.read_csv("KEEN_Yearly.csv")
 features = ["Open", "High", "Low", "Volume", "Adj Close"]
 
 X = df[features].iloc[:-1, :].dropna().values
@@ -18,14 +19,15 @@ y = df["Close"].iloc[1:].dropna().values
 
 # 1 ) Multiple Linear Regression
 
-regressor = LinearRegression()
-regressor.fit(X, y)
-y_predict = regressor.predict(X)
+# regressor = LinearRegression()
+# regressor.fit(X, y)
+# y_predict = regressor.predict(X)
 
-print(np.concatenate((y_predict.reshape(len(y_predict), 1), y.reshape(len(y), 1)), 1))
+# np.set_printoptions(precision=2)
+# print(np.concatenate((y_predict.reshape(len(y_predict), 1), y.reshape(len(y), 1)), 1))
 
-plt.scatter(np.linspace(1, 20, num=20), y[-20:], color="red")
-plt.scatter(np.linspace(1, 20, num=20), y_predict[-20:], color="blue")
+# plt.scatter(np.linspace(1, 20, num=20), y[-20:], color="red")
+# plt.scatter(np.linspace(1, 20, num=20), y_predict[-20:], color="blue")
 
 # 2 ) SVR
 
@@ -42,7 +44,26 @@ plt.scatter(np.linspace(1, 20, num=20), y_predict[-20:], color="blue")
 
 # np.set_printoptions(precision=2)
 # print(np.concatenate((sc_y.inverse_transform(y_predict.reshape(-1, 1)), sc_y.inverse_transform(y)), 1))
+
 # plt.scatter(np.linspace(1, 20, num=20), sc_y.inverse_transform(y).flatten()[-20:], color="red")
 # plt.scatter(np.linspace(1, 20, num=20), sc_y.inverse_transform(y_predict.reshape(-1, 1))[-20:], color="blue")
 
 # print(regressor.predict([[805.00, 815.00, 785.00, 9058400, 805]]))
+
+# 3 Decision Tree
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X_train, y_train)
+
+y_predict = regressor.predict(X_test)
+
+np.set_printoptions(precision=2)
+print(np.concatenate((y_predict.reshape(len(y_predict), 1), y_test.reshape(len(y_test), 1)), 1))
+
+plt.scatter(np.linspace(1, 30, num=30), y_test[-30:], color="red")
+plt.scatter(np.linspace(1, 30, num=30), y_predict[-30:], color="blue")
+
+# Predict future price (Args = Open, High, Low, Volume, Adj Close)
+print(regressor.predict([[805, 815, 785, 9058400, 805]]))
